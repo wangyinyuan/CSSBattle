67a538c99ca8a0eca73ee60e4f4e35b29a5e7e66
Sebastian Wentworth <110143758+wangyinyuan@users.noreply.github.com>
2024-06-30 16:38:22 +0800

    feat: 添加 cookie 处理和路径正则包

--- express-backend/server.js
+++ express-backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use('/', userRoute);  
 
 // 连接到MongoDB数据库
